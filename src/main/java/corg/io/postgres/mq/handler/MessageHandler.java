289a9b3400c0f28200d5c355d8a145b5e5a263b6
Andreas Hailu <andreashailu@gmail.com>
2024-02-20 18:13:26 -0500

    Began MessageHandler tests.

--- src/main/java/corg/io/postgres/mq/handler/MessageHandler.java
+++ src/main/java/corg/io/postgres/mq/handler/MessageHandler.java
@@ -1,9 +1,59 @@
 package corg.io.postgres.mq.handler;
 
+import corg.io.postgres.mq.model.config.DbConfig;
+import corg.io.postgres.mq.model.config.MessageHandlerConfig;
+import corg.io.postgres.mq.model.config.MessageQueueConfig;
 import corg.io.postgres.mq.model.message.Message;
+import corg.io.postgres.mq.model.message.MessageHandlerBatch;
+import corg.io.postgres.mq.table.MessageQueueTable;
+import corg.io.postgres.mq.table.TransactionManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 
-public interface MessageHandler {
-    List<Message> handle(List<Message> messages);
+public class MessageHandler {
+    private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
+
+    private final MessageQueueTable messageQueueTable;
+    private final MessageHandlerConfig messageHandlerConfig;
+    private final TransactionManager transactionManager;
+
+    public static MessageHandler of(DbConfig dbConfig, MessageQueueConfig messageQueueConfig,
+                                                 MessageHandlerConfig messageHandlerConfig) {
+        return new MessageHandler(dbConfig, messageQueueConfig, messageHandlerConfig);
+    }
+
+    private MessageHandler(DbConfig dbConfig, MessageQueueConfig messageQueueConfig,
+                           MessageHandlerConfig messageHandlerConfig) {
+        this.messageQueueTable = MessageQueueTable.of(dbConfig, messageQueueConfig);
+        this.messageHandlerConfig = Objects.requireNonNull(messageHandlerConfig);
+        this.transactionManager = new TransactionManager();
+    }
+
+    public void listen(Function<MessageHandlerBatch, List<Message>> handler) throws SQLException {
+        this.transactionManager.executeInTransaction(() -> {
+            try {
+                return this.messageQueueTable.getConnection();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }, transactionConnection -> {
+            try {
+                var messages = this.messageQueueTable.getPendingMessages(this.messageHandlerConfig.maxNumMessages(), transactionConnection);
+                if(!messages.isEmpty()) {
+                    var handled = handler.apply(MessageHandlerBatch.of(messages, transactionConnection));
+                    this.messageQueueTable.dequeue(handled, transactionConnection);
+                }
+                else {
+                    logger.debug("No messages returned to handle");
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 }
---
+++ src/test/java/corg/io/postgres/mq/handler/MessageHandlerTest.java
@@ -0,0 +1,42 @@
+package corg.io.postgres.mq.handler;
+
+import corg.io.postgres.mq.AbstractMessageQueueTableTest;
+import corg.io.postgres.mq.model.config.DbConfig;
+import corg.io.postgres.mq.model.config.MessageHandlerConfig;
+import corg.io.postgres.mq.model.config.MessageQueueConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public abstract class MessageHandlerTest extends AbstractMessageQueueTableTest {
+    protected MessageHandler messageHandler;
+
+    @BeforeEach
+    public void setupMessageHandlerTest() {
+        var dbConfig = DbConfig.of(getJdbcUrl(), getUserName(), getPassword());
+        var mqConfig = MessageQueueConfig.of(QUEUE_NAME);
+        var mhConfig = MessageHandlerConfig.of(30);
+        this.messageHandler = MessageHandler.of(dbConfig, mqConfig, mhConfig);
+    }
+
+    @Test
+    public void testHandle() {
+        fail("n/i");
+    }
+
+    @Test
+    public void testHandleNoMessages() {
+        fail("n/i");
+    }
+
+    @Test
+    public void testHandleInTransaction() {
+        fail("n/i");
+    }
+
+    @Test
+    public void testHandleInTransactionFailed() {
+        fail("n/i");
+    }
+}
