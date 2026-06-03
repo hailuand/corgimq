2b87e64bf5709d00b2af3ed21316a30b02398872
github-action-benchmark <github@users.noreply.github.com>
2026-06-03 23:19:29 +0000

    add JMH Benchmark (jmh) benchmark result for de49db30d4da8a5f360a0f98b6741125833b18dd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765993916053,
+  "lastUpdate": 1780528769812,
   "repoUrl": "https://github.com/hailuand/corgimq",
   "entries": {
     "JMH Benchmark": [
@@ -298,6 +298,306 @@ window.BENCHMARK_DATA = {
           {
             "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"10\",\"dataSource\":\"POSTGRES\",\"threads\":\"6\"} )",
             "value": 1.9450445415466195,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "6646502+hailuand@users.noreply.github.com",
+            "name": "Andreas Hailu",
+            "username": "hailuand"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "de49db30d4da8a5f360a0f98b6741125833b18dd",
+          "message": "Dependabot auto-approval & merge support (#208)",
+          "timestamp": "2026-06-03T15:42:21-07:00",
+          "tree_id": "867e966c1a12bec966e5ceab7b45f5371eecd10b",
+          "url": "https://github.com/hailuand/corgimq/commit/de49db30d4da8a5f360a0f98b6741125833b18dd"
+        },
+        "date": 1780528769408,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"H2\",\"threads\":\"4\"} )",
+            "value": 0.21592565599503027,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"H2\",\"threads\":\"12\"} )",
+            "value": 0.22726178055560595,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"COCKROACHDB\",\"threads\":\"4\"} )",
+            "value": 9.951406217207126,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"COCKROACHDB\",\"threads\":\"12\"} )",
+            "value": 9.561838011557386,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"MYSQL\",\"threads\":\"4\"} )",
+            "value": 10.629934271290209,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"MYSQL\",\"threads\":\"12\"} )",
+            "value": 10.717274833414894,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"MSSQL\",\"threads\":\"4\"} )",
+            "value": 6.5940861550348275,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"MSSQL\",\"threads\":\"12\"} )",
+            "value": 6.655079315759468,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"ORACLE_FREE\",\"threads\":\"4\"} )",
+            "value": 1.6497886840024734,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"ORACLE_FREE\",\"threads\":\"12\"} )",
+            "value": 1.6688946896549886,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"POSTGRES\",\"threads\":\"4\"} )",
+            "value": 0.5098047957509656,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"10\",\"dataSource\":\"POSTGRES\",\"threads\":\"12\"} )",
+            "value": 0.5079597791965856,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"H2\",\"threads\":\"4\"} )",
+            "value": 2.366779598349554,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"H2\",\"threads\":\"12\"} )",
+            "value": 2.327700914871655,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"COCKROACHDB\",\"threads\":\"4\"} )",
+            "value": 76.49719818864392,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"COCKROACHDB\",\"threads\":\"12\"} )",
+            "value": 76.63823604341128,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"MYSQL\",\"threads\":\"4\"} )",
+            "value": 102.77697033558293,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"MYSQL\",\"threads\":\"12\"} )",
+            "value": 100.81973098970893,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"MSSQL\",\"threads\":\"4\"} )",
+            "value": 50.45070996783594,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"MSSQL\",\"threads\":\"12\"} )",
+            "value": 51.60036116620184,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"ORACLE_FREE\",\"threads\":\"4\"} )",
+            "value": 15.07246314614297,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"ORACLE_FREE\",\"threads\":\"12\"} )",
+            "value": 14.854196212442883,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"POSTGRES\",\"threads\":\"4\"} )",
+            "value": 2.8443098168907843,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushConcurrency.push ( {\"batchSize\":\"100\",\"dataSource\":\"POSTGRES\",\"threads\":\"12\"} )",
+            "value": 2.8830468209785978,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"1\",\"dataSource\":\"H2\"} )",
+            "value": 0.0030590505468230935,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"1\",\"dataSource\":\"COCKROACHDB\"} )",
+            "value": 1.5494831347002853,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"1\",\"dataSource\":\"MYSQL\"} )",
+            "value": 1.4261263326870464,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"1\",\"dataSource\":\"MSSQL\"} )",
+            "value": 0.7569708079573251,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"1\",\"dataSource\":\"ORACLE_FREE\"} )",
+            "value": 1.3084889617711106,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"1\",\"dataSource\":\"POSTGRES\"} )",
+            "value": 0.3331836028517106,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"10\",\"dataSource\":\"H2\"} )",
+            "value": 0.01863789263377933,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"10\",\"dataSource\":\"COCKROACHDB\"} )",
+            "value": 6.104952099052179,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"10\",\"dataSource\":\"MYSQL\"} )",
+            "value": 7.8157593197680955,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"10\",\"dataSource\":\"MSSQL\"} )",
+            "value": 4.180750943140219,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"10\",\"dataSource\":\"ORACLE_FREE\"} )",
+            "value": 1.4434813600378051,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueuePushThroughput.push ( {\"batchSize\":\"10\",\"dataSource\":\"POSTGRES\"} )",
+            "value": 0.409094250251813,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"1\",\"dataSource\":\"H2\",\"threads\":\"6\"} )",
+            "value": 18.72927985115054,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"1\",\"dataSource\":\"COCKROACHDB\",\"threads\":\"6\"} )",
+            "value": 9.428445809548322,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"1\",\"dataSource\":\"MYSQL\",\"threads\":\"6\"} )",
+            "value": 4.623631409490362,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"1\",\"dataSource\":\"ORACLE_FREE\",\"threads\":\"6\"} )",
+            "value": 3.922367120206542,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"1\",\"dataSource\":\"POSTGRES\",\"threads\":\"6\"} )",
+            "value": 5.070225111305916,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"10\",\"dataSource\":\"H2\",\"threads\":\"6\"} )",
+            "value": 0.21288960457052672,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"10\",\"dataSource\":\"COCKROACHDB\",\"threads\":\"6\"} )",
+            "value": 35.02241984875183,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"10\",\"dataSource\":\"MYSQL\",\"threads\":\"6\"} )",
+            "value": 12.419756207559512,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"10\",\"dataSource\":\"ORACLE_FREE\",\"threads\":\"6\"} )",
+            "value": 6.800228418243497,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 1\nthreads: 4"
+          },
+          {
+            "name": "io.github.hailuand.corgi.mq.benchmark.QueueUsage.listen ( {\"batchSize\":\"10\",\"dataSource\":\"POSTGRES\",\"threads\":\"6\"} )",
+            "value": 1.7379694153423841,
             "unit": "ms/op",
             "extra": "iterations: 5\nforks: 1\nthreads: 4"
           }
